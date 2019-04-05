493f6ff992782a1f58813a1e8754be820075e2fa
Frank Drewett <fdrewett@gmail.com>
2019-04-05 14:36:44 -0600

    add if statement for program categories

--- Choco-Franks-Workstation.ps1
+++ Choco-Franks-Workstation.ps1
@@ -1,50 +1,83 @@
 ﻿Set-ExecutionPolicy Unrestricted
 # chocolatey install
 Invoke-Expression ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
-#######################
-# install applications#
-#######################
 
+# Choose the applications to install
+$minimal_install = 0
+$browsers = 1
+$network_utilities = 1
+$windows_utilities = 1
+$office_apps = 1
+$chat = 1
+$programming = 1
+$vmware = 1
+$msp_tools = 0
 # Update the baseline apps
 choco upgrade /y powershell
 choco upgrade /y chocolatey
 
-# Browsers
-choco install /y googlechrome
-choco install /y Firefox
+if ($minimal_install -eq 1) {
+    choco install /y googlechrome
+    choco install /y 7zip
+    choco install /y ccleaner
+} else {
+   # Browsers
+   if ($browsers -eq 1) {
+        choco install /y googlechrome
+        choco install /y Firefox
+   }
 
-# Network utilities
-choco install /y winscp
-choco install /y netscan
-choco install /y wireshark
-choco install /y putty
-choco install /y nmap
+    # Network utilities
+    if ($network_utilities -eq 1) {
+        choco install /y winscp
+        choco install /y netscan
+        choco install /y wireshark
+        choco install /y putty
+        choco install /y nmap
+    }
 
-# Windows utilities
-choco install /y ditto
-choco install /y 7zip
-choco install /y windirstat
-choco install /y sysinternals
-choco install /y procexp
-choco install /y autoruns
-choco install /y ccleaner
-choco install /y disk2vhd
+    # Windows utilities
+    if ($windows_utilities -eq 1) {
+        choco install /y ditto
+        choco install /y 7zip
+        choco install /y windirstat
+        choco install /y sysinternals
+        choco install /y procexp
+        choco install /y autoruns
+        choco install /y ccleaner
+        choco install /y disk2vhd
+    }
+    
+    # Office apps
+    if ($office_apps -eq 1) {
+        choco install /y dropbox
+        choco install /y vlc
+        choco install /y adobereader
+    }
 
+    # Chat
+    if ($chat -eq 1) {
+        choco install /y slack
+        choco install /y microsoft-teams
+    }
 
-# Office apps
-choco install /y dropbox
-choco install /y vlc
-choco install /y adobereader
+    # Programming
+    if ($programming -eq 1) {
+        choco install /y python
+        choco install /y vscode
+        choco install /y autohotkey
+    }
 
-# Chat
-choco install /y slack
-choco install /y microsoft-teams
-
-# Programming
-choco install /y python
-choco install /y vscode
-choco install /y autohotkey
-
-# VMWare
-choco install /y vmwarevsphereclient
-
+    # VMWare
+    if ($vmware -eq 1) {
+        choco install /y vmwarevsphereclient 
+        choco install /y vmwareworkstation 
+    }
+    # MSP Tools
+    if ($msp_tools -eq 1) {
+        choco install /y solarwinds-backup-manager
+        choco install /y solarwinds-recovery-console
+        choco install /y connectwise
+        choco install /y connectwise-manage-client
+    }
+}
